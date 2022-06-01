239b8324d051447b140564c411f63ff4aa022679
Mubarek-Hassen <Mubarekhassen4@gmail.com>
2022-06-01 13:58:01 -0400

    Database Linked

--- main_app/views.py
+++ main_app/views.py
@@ -3,7 +3,7 @@ from django.views import View # <- View class to handle requests
 from django.http import HttpResponse # <- a class to handle sending a type of response
 #...
 from django.views.generic.base import TemplateView
-
+from .models import onechibi
 # Create your views here.
 
 # Here we will be creating a class called Home and extending it from the View class
@@ -15,20 +15,20 @@ class Home(TemplateView):
 class About(TemplateView):
     template_name = "about.html"
 
-class onechibi:
-    def __init__(self, name, image):
-        self.name = name
-        self.image = image
+# class onechibi:
+#     def __init__(self, name, image):
+#         self.name = name
+#         self.image = image
 
-chibi = [
-    onechibi('groot','https://i.pinimg.com/564x/31/d7/33/31d733469baa6e4912a587615896fdd6.jpg'),
-    onechibi('Ghost','https://cdn11.bigcommerce.com/s-2qk6gvu0p8/products/168/images/431/GhostRiley1pc__47989.1635880685.386.513.jpg?c=1')
-]
+# chibi = [
+#     onechibi('groot','https://i.pinimg.com/564x/31/d7/33/31d733469baa6e4912a587615896fdd6.jpg'),
+#     onechibi('Ghost','https://cdn11.bigcommerce.com/s-2qk6gvu0p8/products/168/images/431/GhostRiley1pc__47989.1635880685.386.513.jpg?c=1')
+# ]
 
 class collections(TemplateView):
     template_name = 'collections.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['chibi'] = chibi
+        context['chibi'] = onechibi.objects.all()
         return context
