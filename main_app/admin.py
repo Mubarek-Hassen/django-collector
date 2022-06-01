90a1c9b9c533dc76753c7f0ec5435e6bd4b045c4
Mubarek-Hassen <Mubarekhassen4@gmail.com>
2022-06-01 16:20:45 -0400

    Admin Setup

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import onechibi # import the Artist model from models.py
 # Register your models here.
+
+admin.site.register(onechibi) # this line will add the model to the admin panel
